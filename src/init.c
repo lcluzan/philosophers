2e252d15d5f86fb8177c98c63ee4688bbd9dc4f6
lcluzan <lcluzan@student.42.fr>
2025-03-16 18:18:48 +0100

    change init

--- src/init.c
+++ src/init.c
@@ -5,97 +5,111 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/12 16:16:43 by lcluzan           #+#    #+#             */
-/*   Updated: 2025/03/12 16:18:55 by lcluzan          ###   ########.fr       */
+/*   Created: 2025/03/16 16:11:03 by lcluzan           #+#    #+#             */
+/*   Updated: 2025/03/16 18:11:56 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
-/* Initialise la structure principale */
-int	init_data(t_data *data, int argc, char **argv)
+int	init_mutexes(t_table *table)
 {
-	data->nb_philos = safe_atoi(argv[1]);
-	data->time_to_die = safe_atoi(argv[2]);
-	data->time_to_eat = safe_atoi(argv[3]);
-	data->time_to_sleep = safe_atoi(argv[4]);
-	data->simulation_stop = 0;
+	int	i;
+
+	if (pthread_mutex_init(&table->print_mutex, NULL))
+		return (ERROR);
+	if (pthread_mutex_init(&table->running_mutex, NULL))
+	{
+		pthread_mutex_destroy(&table->print_mutex);
+		return (ERROR);
+	}
+	i = -1;
+	while (++i < table->philo_count)
+	{
+		if (pthread_mutex_init(&table->forks[i].mutex, NULL))
+			return (ERROR);
+		if (pthread_mutex_init(&table->philos[i].meal_mutex, NULL))
+			return (ERROR);
+	}
+	return (SUCCESS);
+}
+
+int	init_forks(t_table *table)
+{
+	int		i;
+	t_fork	*forks;
+
+	forks = (t_fork *)malloc(sizeof(t_fork) * table->philo_count);
+	if (!forks)
+		return (ERROR);
+	table->forks = forks;
+	i = -1;
+	while (++i < table->philo_count)
+	{
+		table->forks[i].id = i;
+		table->forks[i].is_taken = false;
+	}
+	return (SUCCESS);
+}
+
+int	init_philos(t_table *table)
+{
+	int		i;
+	int		next;
+	t_philo	*philos;
+
+	philos = (t_philo *)malloc(sizeof(t_philo) * table->philo_count);
+	if (!philos)
+		return (ERROR);
+	table->philos = philos;
+	i = -1;
+	while (++i < table->philo_count)
+	{
+		next = (i + 1) % table->philo_count;
+		table->philos[i].id = i + 1;
+		table->philos[i].meals_eaten = 0;
+		table->philos[i].last_meal_time = 0;
+		table->philos[i].is_eating = false;
+		table->philos[i].table = table;
+		table->philos[i].left_fork = &table->forks[i];
+		table->philos[i].right_fork = &table->forks[next];
+	}
+	return (SUCCESS);
+}
+
+int	check_args(t_table *table, int argc)
+{
+	if (table->philo_count <= 0 || table->time_to_die <= 0
+		|| table->time_to_eat <= 0 || table->time_to_sleep <= 0
+		|| (argc == 6 && table->must_eat_count <= 0))
+		return (error_exit("Invalid arguments"));
+	return (SUCCESS);
+}
+
+int	init_table(t_table *table, int argc, char **argv)
+{
+	table->philo_count = ft_atoi(argv[1]);
+	table->time_to_die = ft_atoi(argv[2]);
+	table->time_to_eat = ft_atoi(argv[3]);
+	table->time_to_sleep = ft_atoi(argv[4]);
+	table->simulation_running = true;
+	table->must_eat_count = -1;
 	if (argc == 6)
-		data->nb_must_eat = safe_atoi(argv[5]);
-	else
-		data->nb_must_eat = -1;
-	data->philosophers = malloc(sizeof(t_philosopher) * data->nb_philos);
-	if (!data->philosophers)
-		return (0);
-	data->forks = malloc(sizeof(t_fork) * data->nb_philos);
-	if (!data->forks)
+		table->must_eat_count = ft_atoi(argv[5]);
+	if (check_args(table, argc))
+		return (ERROR);
+	if (init_forks(table))
+		return (error_exit("Failed to initialize forks"));
+	if (init_philos(table))
 	{
-		free(data->philosophers);
-		return (0);
+		free(table->forks);
+		return (error_exit("Failed to initialize philosophers"));
 	}
-	return (1);
-}
-
-/* Initialise les fourchettes */
-int	init_forks(t_data *data)
-{
-	int	i;
-
-	i = 0;
-	while (i < data->nb_philos)
+	if (init_mutexes(table))
 	{
-		if (pthread_mutex_init(&(data->forks[i].mutex), NULL) != 0)
-			return (0);
-		data->forks[i].id = i;
-		i++;
+		free(table->forks);
+		free(table->philos);
+		return (error_exit("Failed to initialize mutexes"));
 	}
-	return (1);
-}
-
-/* Initialise les mutex de synchronisation */
-int	init_mutexes(t_data *data)
-{
-	if (pthread_mutex_init(&(data->print_mutex), NULL) != 0)
-		return (0);
-	if (pthread_mutex_init(&(data->meal_mutex), NULL) != 0)
-		return (0);
-	if (pthread_mutex_init(&(data->death_mutex), NULL) != 0)
-		return (0);
-	return (1);
-}
-
-/* Initialise les philosophes */
-void	init_philosophers(t_data *data)
-{
-	int	i;
-
-	i = 0;
-	while (i < data->nb_philos)
-	{
-		data->philosophers[i].id = i + 1;
-		data->philosophers[i].meals_eaten = 0;
-		data->philosophers[i].is_eating = 0;
-		data->philosophers[i].last_meal_time = get_time();
-		data->philosophers[i].data = data;
-		data->philosophers[i].left_fork = &(data->forks[i]);
-		if (i == data->nb_philos - 1)
-			data->philosophers[i].right_fork = &(data->forks[0]);
-		else
-			data->philosophers[i].right_fork = &(data->forks[i + 1]);
-		i++;
-	}
-}
-
-/* Fonction principale d'initialisation */
-int	init_all(t_data *data, int argc, char **argv)
-{
-	if (!init_data(data, argc, argv))
-		return (0);
-	if (!init_forks(data))
-		return (0);
-	if (!init_mutexes(data))
-		return (0);
-	init_philosophers(data);
-	data->start_time = get_time();
-	return (1);
+	return (SUCCESS);
 }
